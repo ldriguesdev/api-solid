ac9b50f4c1ec37f2ccdbde929e74698dedaa189c
Leonardo Rodrigues <ldrigues.dev@gmail.com>
2025-03-02 01:25:17 -0300

    feat: add test should to register

--- src/services/register.spec.ts
+++ src/services/register.spec.ts
@@ -2,8 +2,22 @@ import { describe, expect, it } from 'vitest'
 import { RegisterService } from './register.service'
 import { compare } from 'bcryptjs'
 import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { UserAlreadyExistError } from './errors/user-already-exists-error'
 
 describe('Register Service', () => {
+  it('should to register', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterService(usersRepository)
+
+    const { user } = await registerUseCase.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    expect(user.id).toEqual(expect.any(String))
+  })
+
   it('should hash user password upon registration', async () => {
     const usersRepository = new InMemoryUsersRepository()
     const registerService = new RegisterService(usersRepository)
@@ -14,10 +28,32 @@ describe('Register Service', () => {
       password: '123456',
     })
 
-    console.log(user.password_hash)
-
-    const isPasswordCorrectlyHashed = await compare('12345', user.password_hash)
+    const isPasswordCorrectlyHashed = await compare(
+      '123456',
+      user.password_hash,
+    )
 
     expect(isPasswordCorrectlyHashed).toBe(true)
+  })
+
+  it('should not be able to register with same email twice', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterService(usersRepository)
+
+    const email = 'johndoe@example.com'
+
+    await registerUseCase.execute({
+      name: 'John Doe',
+      email,
+      password: '123456',
+    })
+
+    expect(() =>
+      registerUseCase.execute({
+        name: 'John Doe',
+        email,
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(UserAlreadyExistError)
   })
 })
