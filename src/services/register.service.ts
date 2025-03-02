dc038681786d2dc91ca890d2cc10b5e3bc394f61
Leonardo Rodrigues <ldrigues.dev@gmail.com>
2025-03-02 00:54:16 -0300

    feat: add test password upon registration

--- src/services/register.service.ts
+++ src/services/register.service.ts
@@ -1,6 +1,7 @@
 import type { UsersRepository } from '@/repositories/users-repository'
 import { hash } from 'bcryptjs'
 import { UserAlreadyExistError } from './errors/user-already-exists-error'
+import type { User } from '@prisma/client'
 
 interface RegisterServiceParams {
   name: string
@@ -8,10 +9,18 @@ interface RegisterServiceParams {
   password: string
 }
 
+interface RegisterServiceResponse {
+  user: User
+}
+
 export class RegisterService {
   constructor(private userRepository: UsersRepository) {}
 
-  async execute({ email, name, password }: RegisterServiceParams) {
+  async execute({
+    email,
+    name,
+    password,
+  }: RegisterServiceParams): Promise<RegisterServiceResponse> {
     const password_hash = await hash(password, 6)
 
     const userWithSameEmail = await this.userRepository.findByEmail(email)
@@ -20,10 +29,12 @@ export class RegisterService {
       throw new UserAlreadyExistError()
     }
 
-    await this.userRepository.create({
+    const user = await this.userRepository.create({
       name,
       email,
       password_hash,
     })
+
+    return { user }
   }
 }
--- src/services/register.spec.ts
+++ src/services/register.spec.ts
@@ -1,5 +1,35 @@
-import { expect, test } from 'vitest'
+import { describe, expect, it } from 'vitest'
+import { RegisterService } from './register.service'
+import { compare } from 'bcryptjs'
 
-test('check if it works', () => {
-  expect(2 + 2).toBe(4)
+describe('Register Service', () => {
+  it('should hash user password upon registration', async () => {
+    const registerService = new RegisterService({
+      async findByEmail(email) {
+        return null
+      },
+
+      async create(data) {
+        return {
+          id: 'user-1',
+          name: data.name,
+          email: data.email,
+          password_hash: data.password_hash,
+          created_at: new Date(),
+        }
+      },
+    })
+
+    const { user } = await registerService.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    console.log(user.password_hash)
+
+    const isPasswordCorrectlyHashed = await compare('12345', user.password_hash)
+
+    expect(isPasswordCorrectlyHashed).toBe(true)
+  })
 })
