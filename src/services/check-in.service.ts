c3ac8a3d506d98303571253a51a98aa08047de1b
Leonardo Rodrigues <ldrigues.dev@gmail.com>
2025-03-11 21:50:19 -0300

    feat: add validation data to check-in

--- src/services/check-in.service.ts
+++ src/services/check-in.service.ts
@@ -1,9 +1,13 @@
 import type { CheckIn } from '@prisma/client'
 import type { CheckInsRepository } from '@/repositories/check-ins-repository'
+import type { GymsRepository } from '@/repositories/gyms-repository'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
 
 interface CheckInServiceRequest {
   gymId: string
   userId: string
+  userLatitude: number
+  userLongitude: number
 }
 
 interface CheckInServiceResponse {
@@ -11,12 +15,23 @@ interface CheckInServiceResponse {
 }
 
 export class CheckInService {
-  constructor(private checkInRepository: CheckInsRepository) {}
+  constructor(
+    private checkInRepository: CheckInsRepository,
+    private gymsRepository: GymsRepository,
+  ) {}
 
   async execute({
     userId,
     gymId,
   }: CheckInServiceRequest): Promise<CheckInServiceResponse> {
+    const gym = await this.gymsRepository.findById(gymId)
+
+    if (!gym) {
+      throw new ResourceNotFoundError()
+    }
+
+    // TODO: calculate distance between user and gym
+
     const checkInOnSameDay = await this.checkInRepository.findByUserIdOnDate(
       userId,
       new Date(),
