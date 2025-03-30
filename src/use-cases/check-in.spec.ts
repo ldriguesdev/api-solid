d75447234a81ca67e1fd864427c811b551d3dedd
Leonardo Rodrigues <ldrigues.dev@gmail.com>
2025-03-30 17:20:46 -0300

    feat: add validation check in in same date

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -1,5 +1,4 @@
-import { hash } from 'bcryptjs'
-import { expect, describe, it, beforeEach } from 'vitest'
+import { expect, describe, it, beforeEach, vi, afterEach } from 'vitest'
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
 import { CheckInUseCase } from './check-in'
 
@@ -10,9 +9,49 @@ describe('Authenticate Use Case', () => {
   beforeEach(() => {
     checkInsRepository = new InMemoryCheckInsRepository()
     sut = new CheckInUseCase(checkInsRepository)
+
+    vi.useFakeTimers()
+  })
+
+  afterEach(() => {
+    vi.useRealTimers()
   })
 
   it('should to be able to check in', async () => {
+    const { checkIn } = await sut.execute({
+      gymId: 'gym_id',
+      userId: 'user_id',
+    })
+
+    expect(checkIn.id).toEqual(expect.any(String))
+  })
+
+  it('should to be able to check in twice in the same day', async () => {
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
+
+    await sut.execute({
+      gymId: 'gym-01',
+      userId: 'user-01',
+    })
+
+    await expect(() =>
+      sut.execute({
+        gymId: 'gym-01',
+        userId: 'user-01',
+      }),
+    ).rejects.toBeInstanceOf(Error)
+  })
+
+  it('should to be able to check in twice but in different days', async () => {
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
+
+    await sut.execute({
+      gymId: 'gym_id',
+      userId: 'user_id',
+    })
+
+    vi.setSystemTime(new Date(2022, 0, 21, 8, 0, 0))
+
     const { checkIn } = await sut.execute({
       gymId: 'gym_id',
       userId: 'user_id',
